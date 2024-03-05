c2ef2ef3f3fd71fd355df678383df5b12834f6ff
GhofraneHipto <Ghofrane@hipto.com>
2024-03-05 14:25:39 +0100

    initial commit

--- src/views/authentication/login.jsx
+++ src/views/authentication/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Grid, Button } from '@mui/material';
-import { BASE_URL , api_version } from './config';
+import { BASE_URL, api_version } from './config';
 import logo from '../../assets/images/logos/logo.png';
 import logo_2 from '../../assets/images/logos/logo-2.png';
 import { useNavigate } from 'react-router-dom'; // Importer useNavigate depuis React Router
@@ -16,57 +16,57 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const formdata = new FormData();
-    formdata.append("user_email", email);
-    formdata.append("user_password", password);
+    formdata.append('user_email', email);
+    formdata.append('user_password', password);
     const requestOptions = {
-      method: "POST",
+      method: 'POST',
       body: formdata,
-      redirect: "follow"
+      redirect: 'follow',
     };
     fetch(`${BASE_URL}/${api_version}/token`, requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        return response.text();
-      } else {
-        throw new Error('');
-      }
-    })
-    .then((token) => {
-      localStorage.setItem('token', token);
-      // Afficher la boîte de dialogue de succès avec le bouton "Ok" masqué
-      Swal.fire({
-        icon: 'success',
-        text: 'Top, connexion établie!',
-        customClass: {
-          popup: 'my-custom-modal',
-        },
-        width: '30%', // Définir la largeur du modal
-        showConfirmButton: false, // Masquer le bouton "Ok"
-        timer: 1000, // Fermer automatiquement après 10 secondes
-        //timerProgressBar: true // Afficher une barre de progression du temps restant
-      }).then(() => {
-        navigate('/Pioche');
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error('');
+        }
+      })
+      .then((token) => {
+        localStorage.setItem('token', token);
+        // Afficher la boîte de dialogue de succès avec le bouton "Ok" masqué
+        Swal.fire({
+          icon: 'success',
+          text: 'Top, connexion établie!',
+          customClass: {
+            popup: 'my-custom-modal',
+          },
+          width: '30%', // Définir la largeur du modal
+          showConfirmButton: false, // Masquer le bouton "Ok"
+          timer: 1000, // Fermer automatiquement après 10 secondes
+          //timerProgressBar: true // Afficher une barre de progression du temps restant
+        }).then(() => {
+          navigate('/Pioche');
+        });
+      })
+      .catch((error) => {
+        // Afficher la boîte de dialogue d'erreur
+        Swal.fire({
+          icon: 'error',
+          //title: 'Erreur de connexion',
+          text: 'Merci de vérifier vos paramètres de connexion!',
+          width: '30%',
+          confirmButtonText: "Ok, j'ai compris!",
+          confirmButtonColor: '#0095E8',
+        });
+        setError(error.message);
       });
-    })
-    .catch((error) => {
-      // Afficher la boîte de dialogue d'erreur
-      Swal.fire({
-        icon: 'error',
-        //title: 'Erreur de connexion',
-        text: 'Merci de vérifier vos paramètres de connexion!',
-        width: '30%',
-        confirmButtonText: "Ok, j'ai compris!",
-        confirmButtonColor: '#0095E8'
-      });
-      setError(error.message);
-    });
   };
   return (
     <Grid container className="centered-container">
       {/* Begin:: card canal */}
       <Grid item xs={6} className="grid-left centered-item">
         {/* begin::Logo */}
-          <img alt="Logo" src={logo} className="logo-1" />
+        <img alt="Logo" src={logo} className="logo-1" />
         {/* end::Logo */}
         {/* begin::Title */}
         <h1 className="titre-1">Connexion à votre compte</h1>
@@ -76,7 +76,6 @@ const Login = () => {
           <span className="text">...et bon travail</span>
         </div>
         {/* begin::Form */}
-        {error && <p className="message">{error}</p>}
         <Box>
           <Form className="form" onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -108,7 +107,7 @@ const Login = () => {
       </Grid>
       <Grid item xs={6} className="grid-right centered-item">
         {/* begin::Logo */}
-          <img alt="Logo" className="logo-2" src={logo_2} />
+        <img alt="Logo" className="logo-2" src={logo_2} />
         {/* end::Logo */}
         {/* begin::Title */}
         <h1 className="titre-2">The lead generation 🧢</h1>
