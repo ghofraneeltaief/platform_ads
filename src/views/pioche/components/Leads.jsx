89b1c3cf87e9532ef1a665c4a61b6dc1036b63b0
GhofraneHipto <Ghofrane@hipto.com>
2024-03-18 11:24:57 +0100

    initial commit

--- src/views/pioche/components/Leads.jsx
+++ src/views/pioche/components/Leads.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   Typography,
   Box,
@@ -22,145 +22,113 @@ function Leads({
   onSourceCount,
   tableDataVide,
 }) {
-  /* Begin: getToken */
-  async function getToken() {
+  const getToken = async () => {
     const token = localStorage.getItem('token');
     if (token) {
       return token;
     } else {
       throw new Error('No token available');
     }
-  }
-  /* End: getToken */
-  /* Begin: fetchTableHeaders fetchVide */
+  };
+
   const [tableHeaders, setTableHeaders] = useState([]);
   const [tableData, setTableData] = useState([]);
-  const formdata = new FormData();
-  useEffect(() => {
-    const fetchTableHeaders = async () => {
-      if (selectedVerticalId && selectedDateFrom && selectedDateTo) {
-        try {
-          const token = await getToken();
-          const responseObject = JSON.parse(token);
-          const accessToken = responseObject.access_token;
-          formdata.append('Hipto-Authorization', accessToken);
-          const requestOptions = {
-            method: 'POST',
-            body: formdata,
-          };
-          const response = await fetch(
-            `${BASE_URL}/${api_version}/pioche/headers?vertical_id=${selectedVerticalId}&from=${selectedDateFrom}&to=${selectedDateTo}`,
-            requestOptions,
-          );
-          const data = await response.json();
-          setTableHeaders(data);
-          // Mettre à jour l'état avec les en-têtes de colonne récupérés depuis l'API
-        } catch (error) {
-          Swal.fire({
-            icon: 'error',
-            //title: 'Erreur de connexion',
-            text: 'Erreur lors de la récupération des en-têtes de colonne!',
-            width: '30%',
-            confirmButtonText: "Ok, j'ai compris!",
-            confirmButtonColor: '#0095E8',
-          });
-          setError('Erreur lors de la récupération des en-têtes de colonne');
-        }
-      }
-    };
-    /* Begin: fetchTableHeaders */
-    /* Begin: fetchTableData */
-    const fetchTableData = async () => {
-      if (selectedVerticalId && selectedDateFrom && selectedDateTo) {
-        try {
-          const token = await getToken();
-          const responseObject = JSON.parse(token);
-          const accessToken = responseObject.access_token;
-          formdata.append('Hipto-Authorization', accessToken);
-          const requestOptions = {
-            method: 'POST',
-            body: formdata,
-          };
-          const dataResponse = await fetch(
-            `${BASE_URL}/${api_version}/pioche/datas?vertical_id=${selectedVerticalId}&from=${selectedDateFrom}&to=${selectedDateTo}`,
-            requestOptions,
-          );
-          const data = await dataResponse.json();
-          setTableData(data);
-          // Compter le nombre d'occurrences pour chaque valeur de Source
-          const countsSource = {};
-          data.forEach((item) => {
-            if (item.utm_source !== null) {
-              const Source = item.utm_source;
-              if (countsSource[Source]) {
-                countsSource[Source] += 1;
-              } else {
-                countsSource[Source] = 1;
-              }
-            } else {
-              // Si utm_source est null, changer la valeur à "Unorganized"
-              const Source = 'Unorganized';
-              if (countsSource[Source]) {
-                countsSource[Source] += 1;
-              } else {
-                countsSource[Source] = 1;
-              }
-            }
-          });
-          onSourceCount(countsSource);
-          // Compter le nombre d'occurrences pour chaque valeur de canal
-          const counts = {};
-          data.forEach((item) => {
-            const canal = item.canal;
-            if (counts[canal]) {
-              counts[canal] += 1;
-            } else {
-              counts[canal] = 1;
-            }
-          });
-          onCanalCount(counts);
-        } catch (error) {
-          // Afficher la boîte de dialogue d'erreur
-          Swal.fire({
-            icon: 'error',
-            //title: 'Erreur de connexion',
-            text: 'Erreur lors de la récupération des données !',
-            width: '30%',
-            confirmButtonText: "Ok, j'ai compris!",
-            confirmButtonColor: '#0095E8',
-          });
-          setError('Erreur lors de la récupération des données.');
-        }
-      }
-    };
-    /* End: fetchTableData */
-    fetchTableHeaders();
-    fetchTableData();
-  }, [selectedVerticalId, selectedDateFrom, selectedDateTo]);
-  /* End: fetchTableData fetchTableHeaders */
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [error, setError] = useState(null);
 
-  /* Begin: fonction mettre le tableau vide */
+  const fetchTableHeaders = useCallback(async () => {
+    try {
+      const token = await getToken();
+      const accessToken = JSON.parse(token).access_token;
+      const formdata = new FormData();
+      formdata.append('Hipto-Authorization', accessToken);
+      const requestOptions = {
+        method: 'POST',
+        body: formdata,
+      };
+      const response = await fetch(
+        `${BASE_URL}/${api_version}/pioche/headers?vertical_id=${selectedVerticalId}&from=${selectedDateFrom}&to=${selectedDateTo}`,
+        requestOptions,
+      );
+      const data = await response.json();
+      setTableHeaders(data);
+    } catch (error) {
+      handleFetchError('Erreur lors de la récupération des en-têtes de colonne!');
+    }
+  }, [selectedVerticalId, selectedDateFrom, selectedDateTo]);
+
+  const fetchTableData = useCallback(async () => {
+    try {
+      const token = await getToken();
+      const accessToken = JSON.parse(token).access_token;
+      const formdata = new FormData();
+      formdata.append('Hipto-Authorization', accessToken);
+      const requestOptions = {
+        method: 'POST',
+        body: formdata,
+      };
+      const response = await fetch(
+        `${BASE_URL}/${api_version}/pioche/datas?vertical_id=${selectedVerticalId}&from=${selectedDateFrom}&to=${selectedDateTo}`,
+        requestOptions,
+      );
+      const data = await response.json();
+      setTableData(data);
+      const countsSource = {};
+      const counts = {};
+      data.forEach((item) => {
+        const source = item.utm_source || 'Unorganized';
+        countsSource[source] = (countsSource[source] || 0) + 1;
+        counts[item.canal] = (counts[item.canal] || 0) + 1;
+      });
+      onSourceCount(countsSource);
+      onCanalCount(counts);
+    } catch (error) {
+      handleFetchError('Erreur lors de la récupération des données !');
+    }
+  }, [selectedVerticalId, selectedDateFrom, selectedDateTo, onSourceCount, onCanalCount]);
+
+  const handleFetchError = (errorMessage) => {
+    Swal.fire({
+      icon: 'error',
+      text: errorMessage,
+      width: '30%',
+      confirmButtonText: "Ok, j'ai compris!",
+      confirmButtonColor: '#0095E8',
+    });
+    setError(errorMessage);
+  };
+
   useEffect(() => {
-    // Mettre à jour l'état du tableau avec les nouvelles données passées depuis Pioche
+    if (selectedVerticalId && selectedDateFrom && selectedDateTo) {
+      Promise.all([fetchTableHeaders(), fetchTableData()])
+        .catch((error) => handleFetchError('Erreur lors de la récupération des données !'));
+    }
+  }, [selectedVerticalId, selectedDateFrom, selectedDateTo]);
+
+  useEffect(() => {
     setTableData(tableDataVide);
     setTableHeaders(tableDataVide);
   }, [tableDataVide]);
-  /* End: fonction mettre le tableau vide */
 
-  /* Begin: Pagination Table */
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
+
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-  /* End: Pagination Table */
-
-  const [error, setError] = useState(null);
-
+  
+  const renderTableHeaders = () => {
+    return tableHeaders.map((header) => (
+      <TableCell key={header} sx={{ backgroundColor: '#A367DC', color: 'white' }}>
+        <Typography variant="subtitle2" fontWeight={600}>
+          {header}
+        </Typography>
+      </TableCell>
+    ));
+  };
   return (
     <DashboardCard>
       <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
@@ -198,15 +166,7 @@ function Leads({
         <TextField id="outlined-basic" label="Rechercher" variant="outlined" />
       </Box>
       <Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
-        {/* Begin:: table */}
-        <Table
-          aria-label="simple table"
-          sx={{
-            whiteSpace: 'nowrap',
-            mt: 2,
-          }}
-        >
-          {/* Begin:: table head */}
+        <Table aria-label="simple table" sx={{ whiteSpace: 'nowrap', mt: 2 }}>
           <TableHead>
             <TableRow>
               <TableCell sx={{ backgroundColor: '#67B7DC', color: 'white' }}>
@@ -244,14 +204,7 @@ function Leads({
                   Téléphone
                 </Typography>
               </TableCell>
-              {/* Générer dynamiquement les balises th en fonction des données d'en-tête */}
-              {tableHeaders.map((header) => (
-                <TableCell sx={{ backgroundColor: '#A367DC', color: 'white' }} key={header}>
-                  <Typography variant="subtitle2" fontWeight={600}>
-                    {header}
-                  </Typography>
-                </TableCell>
-              ))}
+              {renderTableHeaders()}
               <TableCell sx={{ backgroundColor: '#DC67AB', color: 'white' }}>
                 <Typography variant="subtitle2" fontWeight={600}>
                   Canal
@@ -289,8 +242,6 @@ function Leads({
               </TableCell>
             </TableRow>
           </TableHead>
-          {/* End:: table head */}
-          {/* Begin:: table body */}
           <TableBody>
             {tableData
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -319,19 +270,12 @@ function Leads({
                   </TableCell>
                   {tableHeaders.map((header) => (
                     <TableCell key={header}>
-                      <Box
-                        sx={{
-                          display: 'flex',
-                          alignItems: 'center',
-                        }}
-                      >
+                      <Box sx={{ display: 'flex', alignItems: 'center' }}>
                         <Box>
                           <Typography variant="subtitle2"></Typography>
                           <Typography
                             color="textSecondary"
-                            sx={{
-                              fontSize: '13px',
-                            }}
+                            sx={{ fontSize: '13px' }}
                           >
                             {row[header]}
                           </Typography>
@@ -363,9 +307,7 @@ function Leads({
                 </TableRow>
               ))}
           </TableBody>
-          {/* End:: table Body*/}
         </Table>
-        {/* End:: table */}
       </Box>
       <TablePagination
         rowsPerPageOptions={[10, 25, 50]}
